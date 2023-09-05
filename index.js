a68b2a7b281bbf5b5437387d87f98e9e42462f5d
Imran Malik <Imranmlk14@gmail.com>
2023-09-04 21:23:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+use add subtract and multiply to print combination 10-40-39
+*/
+// print out greeting
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+// variables for each equation
+let num_a = 5+5
+let num_b = 20*2
+let num_c = 40-1
+//print out of combination using varibales
+alert(num_a + "-" + num_b + "-" + num_c)
